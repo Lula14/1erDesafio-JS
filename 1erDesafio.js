f8f31524f7fdc401678f8b165b25096966b5f03f
Maria L Iglesias <106163736+Lula14@users.noreply.github.com>
2022-08-03 14:08:08 -0300

    Add files via upload

--- 1erDesafio.js
+++ 1erDesafio.js
@@ -6,7 +6,7 @@ if (edadUsuario >= "18") {
 else if (edadUsuario < "18") {
     alert ("Lo sentimos, eres demasiado joven para este sitio.")
 console.log ("No ingresa por ser menor de edad")}
-//debugger
+/*
 for (let i = 1; i <= 10; i++) {
     let resultado = edadUsuario + i ;
     alert(edadUsuario +" X "+ i +" = "+ resultado);}
@@ -18,4 +18,29 @@ for (let i = 1; i <= 10; i++) {
         edadUsuario = prompt("Ingresar otro dato");
         console.log ("La edad ingresada es menor a 18")
     }
+*/
+    let nombreUsuario = (prompt("Ingrese su nombre"));
+    function verNombreUsuario () {
+        console.log ("Bienvenid@", nombreUsuario)
+        alert ("Bienvenid@" , nombreUsuario)
+    }
+    
+    let calcularEdadUsuario = (prompt("Ingrese su año de nacimiento, ejemplo ´1995´"))
+        function calcularEdad () {
+          let anioActual = prompt ("Ingrese el año actual")
+          let anioNacimiento = prompt ("Ingrese su año de nacimiento")
+          let operador = prompt ("Seleccione resta: '-'")
+          console.log ("Resultado",  realizarCalculo (anioActual , anioNacimiento , operador))
+          
+    }
+    
+    function realizarCalculo (num1, num2, ope) {
+        switch (ope) {
+            case "-":
+                return num2 - num1
+            default:
+                return "Error en el calculo"
+       }
+    }
+   
     
